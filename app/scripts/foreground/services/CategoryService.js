0b9b79fd94f3cf87b24abdbeb65405e11c972d04
Anh Trinh <anh@catalyst.net.nz>
2015-04-08 13:34:10 +1200

    first commit for chrome app

---
+++ app/scripts/foreground/services/CategoryService.js
@@ -0,0 +1,63 @@
+(function(){
+    'use strict';
+
+    angular.module('soundCloudify')
+        .service("Category", CategoryService);
+
+    function CategoryService($http, CLIENT_ID, TrackAdapter, $q){
+
+        var cachedCategory;
+
+        chrome.storage.local.get('charts', function (result) {
+            cachedCategory = result.charts || [];
+        });
+        
+        return {
+            getList: getList,
+            getTracks: getTracks
+        };
+
+        function getList(){
+
+            return $q(function(resolve, reject) {
+
+                if (cachedCategory.length) {
+                    resolve(cachedCategory);
+                } else {
+                    var params = { limit: 10, offset: 0, linked_partitioning: 1, client_id: CLIENT_ID };
+                    $http.get('https://api-v2.soundcloud.com/explore/categories', { params: params })
+                    .success(function(data) {
+                        cachedCategory = data['music'] || [];
+                        resolve(cachedCategory);
+                        
+                        chrome.storage.local.set({'charts': cachedCategory});
+                    });
+                }
+            });
+
+        }
+
+        function getTracks(category, pagingObject) {
+            var params = { limit: pagingObject.limit, offset: pagingObject.skip, linked_partitioning: 1, client_id: CLIENT_ID };
+
+            return $q(function(resolve, reject) {
+                $http({
+                    url: 'https://api-v2.soundcloud.com/explore/' + category,
+                    method: 'GET',
+                    params: params,
+                    transformResponse: ServiceHelpers.appendTransform($http.defaults.transformResponse, function(result) {
+                        if (!result || !result.tracks) return [];
+                        return {
+                            tracks: TrackAdapter.adaptMultiple(result.tracks, 'sc')
+                        };
+                    })
+                }).success(function(data) {
+                    resolve(data);
+                }).error(function() {
+                    reject();
+                });
+            });
+        }
+    };
+
+}());
---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Listens for the app launching then creates the window
+chrome.app.runtime.onLaunched.addListener(function() {
+  var width = 660;
+  var height = 448;
+
+  chrome.app.window.create('index.html', {
+    id: 'main',
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screen.availWidth - width) / 2),
+      top: Math.round((screen.availHeight - height)/2)
+    }
+  });
+});
